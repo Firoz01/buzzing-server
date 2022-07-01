e8a9abb522d85cbd621ca9d975d39ab177cc1023
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-07-01 19:58:29 +0600

    update and delete route done

--- Controllers/UserControlller.js
+++ Controllers/UserControlller.js
@@ -1,5 +1,8 @@
-/* eslint-disable import/prefer-default-export */
 /* eslint-disable import/extensions */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import bcrypt from 'bcrypt';
+
 import UserModel from '../Models/userModel.js';
 
 export const getUser = async (req, res) => {
@@ -14,5 +17,42 @@ export const getUser = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currenUserAdminStatus, password } = req.body;
+
+  if (id === currentUserId || currenUserAdminStatus) {
+    try {
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res.status(401).json('Access Denied!');
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currenUserAdminStatus } = req.body;
+  if (currentUserId || currenUserAdminStatus) {
+    try {
+      await UserModel.findByIdAndDelete(id);
+      res.status(200).json('User deleted successfully');
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res.status(401).json('Access Denied!');
   }
 };
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,9 +1,15 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { getUser } from '../Controllers/UserControlller.js';
+import {
+  deleteUser,
+  getUser,
+  updateUser
+} from '../Controllers/UserControlller.js';
 
 const router = express.Router();
 
 router.get('/:id', getUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
