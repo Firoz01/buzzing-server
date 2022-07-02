1d96d50892b10b7ac57cf0224adf8d6f74859659
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-07-02 23:50:50 +0600

    adding post route update delete

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -20,3 +20,34 @@ export const getPost = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('post updated successfully!');
+    } else {
+      res.status(401).json("You can't update in this post");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json('post delete successfully') ;
+    } else {
+      res.status(401).json("You can't delete in this post");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
