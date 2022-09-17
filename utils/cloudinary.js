65e3de955acef451027756a7e4d775b44b6a78e6
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-09-17 23:47:18 +0600

    change cloudinary with env

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,14 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
 cloudinary.config({
-  cloud_name: 'suvrow9',
-  api_key: '542411119464244',
-  api_secret: 'hNKO0Ds4ybcYrKy2WHPKvTkvV7E'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 
 export default cloudinary;
