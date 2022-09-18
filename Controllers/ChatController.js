6a0648baffc57ef2c15c82b2a825ef742f441928
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-09-18 23:54:10 +0600

    create userChats controller

--- Controllers/ChatController.js
+++ Controllers/ChatController.js
@@ -9,3 +9,14 @@ export const createChat = catchAsync(async (req, res) => {
   const result = await newChat.save();
   res.status(200).json(result);
 });
+
+export const userChats = async (req, res) => {
+  try {
+    const chat = await ChatModel.find({
+      members: { $in: [req.params.userId] }
+    });
+    res.status(200).json(chat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
