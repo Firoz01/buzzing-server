8a2d701b771e5fa5ab71f1bb32bb0554eb72509f
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-10-04 22:07:39 +0600

    chat controller working fine

--- Controllers/v1/ChatController.js
+++ Controllers/v1/ChatController.js
@@ -24,7 +24,7 @@ export const userChats = async (req, res) => {
 export const findChat = async (req, res) => {
   try {
     const chat = await ChatModel.findOne({
-      members: { $all: [req.params.fistId, req.params.secondId] }
+      members: { $all: [req.params.firstId, req.params.secondId] }
     });
 
     res.status(200).json(chat);
--- MiddleWare/authMiddleWare.js
+++ MiddleWare/authMiddleWare.js
@@ -6,12 +6,10 @@ dotenv.config({ path: './config.env' });
 
 const secret = process.env.JWT_KEY;
 
-console.log("Secrect token",secret);
 
 const authMiddleWare = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    //console.log("token",token)
     if (token) {
       const decoded = jwt.verify(token, secret);
       req.body._id = decoded?.id;
